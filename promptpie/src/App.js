4063e6d020c4b01f40b2f226ce5d3149bdbd6b6d
vijaya varshini <vijayavarshini2004@gmail.com>
2024-08-20 07:25:46 +0530

    'homepage'

--- promptpie/src/App.js
+++ promptpie/src/App.js
@@ -1,14 +1,17 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
+import Home from './Home'
+// import LoginPage from './LoginPage'
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+        {/* <Route path="/" element={<LoginPage/>} /> */}
+        </Routes>
     </Router>
   );
 }
